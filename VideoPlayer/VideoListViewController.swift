27af3aa9df1412efaa167ea1e958ab918a8e6399
hiroshi oshiro <ooshiro010@gmail.com>
2019-06-24 23:45:27 +0900

    Added http access and listing

--- VideoPlayer/VideoListViewController.swift
+++ VideoPlayer/VideoListViewController.swift
@@ -12,7 +12,13 @@ class VideoListViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
-    var videoInfoList: [VideoInfo]?
+    var videoInfoList: [VideoInfo]? {
+        didSet {
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,13 +27,42 @@ class VideoListViewController: UIViewController {
         tableView.rowHeight = UITableView.automaticDimension
         tableView.dataSource = self
         tableView.delegate = self
+        
+        if let url = URL(string: "https://quipper.github.io/native-technical-exam/playlist.json") {
+            getVideoInfo(url: url, completion: { (videos) in
+                self.videoInfoList = videos
+            })
+        }
+    }
+    
+    func getVideoInfo(url: URL, completion: @escaping ([VideoInfo]) -> Swift.Void) {
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            // check error
+            if let e = error {
+                print(e.localizedDescription)
+                return
+            }
+            // check response
+            if let r = response as? HTTPURLResponse,
+                r.statusCode == 200,
+                let d = data {
+                let decoder = JSONDecoder()
+                do {
+                    let videos = try decoder.decode([VideoInfo].self, from: d)
+                    completion(videos)
+                } catch {
+                    print("error: ", error.localizedDescription)
+                }
+            }
+        }
+        task.resume()
     }
 }
 
 // TableView
 extension VideoListViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return videoInfoList?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -42,6 +77,7 @@ extension VideoListViewController: UITableViewDelegate, UITableViewDataSource {
         return cell
     }
     
-    
-    
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return 200
+//    }
 }
