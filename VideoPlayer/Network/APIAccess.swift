7f002453165fdc633e5fc415c220bf81469dffb2
hiroshi <hiroshi@veldt.jp>
2019-06-30 16:05:06 +0900

    create APIAccess class and separate related code

---
+++ VideoPlayer/Network/APIAccess.swift
@@ -0,0 +1,41 @@
+//
+//  APIAccess.swift
+//  VideoPlayer
+//
+//  Created by Hiroshi Oshiro on 2019/06/30.
+//  Copyright © 2019 hiroshi. All rights reserved.
+//
+
+import Foundation
+
+class APIAccess {
+    
+    static private let baseURL = "https://quipper.github.io/native-technical-exam/playlist.json"
+    
+    static func getVideoInfo(completion: @escaping ([VideoInfo]) -> Void) {
+        guard let url = URL(string: baseURL) else {
+            return
+        }
+
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            // check error
+            if let e = error {
+                print(e.localizedDescription)
+                return
+            }
+            // check response
+            if let r = response as? HTTPURLResponse,
+                r.statusCode == 200,
+                let d = data {
+                let decoder = JSONDecoder()
+                do {
+                    let videos = try decoder.decode([VideoInfo].self, from: d)
+                    completion(videos)
+                } catch {
+                    print("error: ", error.localizedDescription)
+                }
+            }
+        }
+        task.resume()
+    }
+}
--- VideoPlayer/VideoListViewController.swift
+++ VideoPlayer/VideoListViewController.swift
@@ -12,7 +12,7 @@ class VideoListViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
-    var videoInfoList: [VideoInfo]? {
+    var videos: [VideoInfo]? {
         didSet {
             DispatchQueue.main.async {
                 self.tableView.reloadData()
@@ -23,46 +23,30 @@ class VideoListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setTable()
+        
+        APIAccess.getVideoInfo(completion: { (videos) in
+            self.videos = videos
+        })
+    }
+    
+        }
+    }
+    
+        }
+    }
+    private func setTable() {
         tableView.estimatedRowHeight = 100
         tableView.rowHeight = UITableView.automaticDimension
         tableView.dataSource = self
         tableView.delegate = self
-        
-        if let url = URL(string: "https://quipper.github.io/native-technical-exam/playlist.json") {
-            getVideoInfo(url: url, completion: { (videos) in
-                self.videoInfoList = videos
-            })
-        }
-    }
-    
-    func getVideoInfo(url: URL, completion: @escaping ([VideoInfo]) -> Swift.Void) {
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
-            // check error
-            if let e = error {
-                print(e.localizedDescription)
-                return
-            }
-            // check response
-            if let r = response as? HTTPURLResponse,
-                r.statusCode == 200,
-                let d = data {
-                let decoder = JSONDecoder()
-                do {
-                    let videos = try decoder.decode([VideoInfo].self, from: d)
-                    completion(videos)
-                } catch {
-                    print("error: ", error.localizedDescription)
-                }
-            }
-        }
-        task.resume()
     }
 }
 
 // TableView
 extension VideoListViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return videoInfoList?.count ?? 0
+        return videos?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -70,10 +54,14 @@ extension VideoListViewController: UITableViewDelegate, UITableViewDataSource {
             fatalError("Cell identifer is not correct!")
         }
         
-        if let videoInfo = videoInfoList?[indexPath.row] {
+        if let videoInfo = videos?[indexPath.row] {
             cell.setUpWithData(data: videoInfo)
         }
         
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
     }
 }
